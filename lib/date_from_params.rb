f8a11bcb86b677b7bb9865b5ba9e3d37cbe27c3a
Pavel Astraukh <paladin111333@yandex.ru>
2014-01-22 13:35:41 +0300

    Add parse logic
    Signed-off-by: Pavel Astraukh <paladin111333@yandex.ru>

--- lib/date_from_params.rb
+++ lib/date_from_params.rb
@@ -1,5 +1,45 @@
 require "date_from_params/version"
 
 module DateFromParams
-  # Your code goes here...
+  def date_from_select_date(key, params)
+    date_params = params[key]
+
+    begin
+      Date.civil(*[:year, :month, :day].map { |k| date_params[k].to_i unless date_params[k].blank? })
+    rescue
+      nil
+    end
+  end
+
+  def datetime_from_select_datetime(key, params)
+    datetime_params = params[key]
+
+    begin
+      DateTime.civil(*[:year, :month, :day, :hour, :minute].map { |k| datetime_params[k].to_i unless datetime_params[k].blank? })
+    rescue
+      nil
+    end
+  end
+
+  def date_from_date_select(key, params)
+    date_params = params.select { |k,v| k.include?(key.to_s) }
+
+    begin
+      Date.civil(*date_params.values.map { |v| v.to_i unless v.blank? })
+    rescue
+      nil
+    end
+  end
+
+  def datetime_from_datetime_select(key, params)
+    datetime_params = params.select { |k,v| k.include?(key.to_s) }
+
+    begin
+      DateTime.civil(*datetime_params.values.map { |v| v.to_i unless v.blank? })
+    rescue
+      nil
+    end
+  end
+
+  extend self
 end
